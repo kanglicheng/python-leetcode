1aa6f2f85693fc27cf4e103055e7c8c9036280a6
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2017-03-29 00:49:59 -0400

    added comments

--- minimumPathSum.py
+++ minimumPathSum.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        m = len(grid)
+        m = len(grid) #get the dimensions 
         n = len(grid[0])
-        for i in range(1, n):
+        for i in range(1, n):   #populate the first row of the grid with consecutive sums across
             grid[0][i] += grid[0][i-1]
-        for i in range(1, m):
+        for i in range(1, m):   #replacing the first column of the grid with consecutive sums
             grid[i][0] += grid[i-1][0]
-        for i in range(1, m):
+        for i in range(1, m):   #fill in the rest of the grid with the minimum sum to reach it.
             for j in range(1, n):
                 grid[i][j] += min(grid[i-1][j], grid[i][j-1])
-        return grid[-1][-1]
+        return grid[-1][-1] #the top left element contains the solution
