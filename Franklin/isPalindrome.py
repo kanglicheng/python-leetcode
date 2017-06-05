a0948b4714261f39a5bd62b42366a045d334d5bc
Franklin Qin <zq29@cornell.edu>
2017-06-05 14:26:34 -0400

    Create isPalindrome.py

---
+++ Franklin/isPalindrome.py
@@ -0,0 +1,16 @@
+# lc problem 9
+# Determine whether an integer is a palindrome.
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if (x<0 or ()):
+            return False
+        rev = 0
+        while (x!=0):
+            rev = rev*10 + x%10
+            x = x/10
+        return (x==rev or x==rev/10)
