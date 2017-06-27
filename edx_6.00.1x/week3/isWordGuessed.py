1b999dcde6691db3d80205ca8cd7c140cf59c743
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2017-06-27 15:08:28 -0400

    Create isWordGuessed.py

---
+++ edx_6.00.1x/week3/isWordGuessed.py
@@ -0,0 +1,30 @@
+'''
+Problem 1
+Problem 1 - Is the Word Guessed
+10.0/10.0 points (graded)
+Please read the Hangman Introduction before starting this problem. We'll start by writing 3 simple functions that will help us easily code the Hangman problem. First, implement the function isWordGuessed that takes in two parameters - a string, secretWord, and a list of letters, lettersGuessed. This function returns a boolean - True if secretWord has been guessed (ie, all the letters of secretWord are in lettersGuessed) and False otherwise.
+
+Example Usage:
+
+>>> secretWord = 'apple' 
+>>> lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
+>>> print(isWordGuessed(secretWord, lettersGuessed))
+False
+'''
+For this function, you may assume that all the letters in secretWord and lettersGuessed are lowercase.
+
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+    # FILL IN YOUR CODE HERE...
+    d= {}
+    for letter in lettersGuessed:
+        d[letter]=1 if letter not in d else d[letter]+1
+    for l in secretWord:
+        if l not in d:
+            return False
+    return True
