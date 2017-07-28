fe3bbdef8c5ed8b3c5d427fac26e33d2cce04fe8
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2017-07-28 14:58:49 -0400

    add cipher

--- edx_6.00.1x/final_exam/final_exam.py
+++ edx_6.00.1x/final_exam/final_exam.py
@@ -37,5 +37,28 @@ def _max_val(t, ans):
 
 print ( max_val(((2, 3, 4), (5, 6), [10, [11]])))
 
+# Question 5
+"""
+def cipher(map_from, map_to, code):
+     map_from, map_to: strings where each contain
+                          N unique lowercase letters.
+        code: string (assume it only contains letters also in map_from)
+        Returns a tuple of (key_code, decoded).
+        key_code is a dictionary with N keys mapping str to str where
+        each key is a letter in map_from at index i and the corresponding
+        value is the letter in map_to at index i.
+        decoded is a string that contains the decoded version
+        of code using the key_code mapping.
+    # Your code here
+"""
+def cipher(map_from, map_to, code):
+    d ={}
+    ans =""
+    for i, c in enumerate(map_from):
+        d[c]= map_to[i]
+
+    for c in code:
+        ans += d[c]
+    return (d, ans)
 
 #print (max_val(([[2, 1, 5], [4, 2], 6], ([2, 1], (7, 7, 5), 6))))
