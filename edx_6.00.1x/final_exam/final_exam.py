ae0e2c09d7e1a44250089edc53f93112cf77f5f3
Steven Cheng <kangli.cheng@gmail.com>
2017-07-28 15:29:35 -0400

    add question 4

--- edx_6.00.1x/final_exam/final_exam.py
+++ edx_6.00.1x/final_exam/final_exam.py
@@ -22,18 +22,19 @@ print (max_val((5, [1, 9]) ))
 
 def max_val(t):
     ans =0
-    _max_val(t, ans)
+    results=[]
+    _max_val(t, ans, results)
+    return max(results)
 
-def _max_val(t, ans):
+
+def _max_val(t, ans, results):
     for c in t:
         if type(c) != int:
-            max_val(c)
+            _max_val(c, ans, results)
         elif c > ans:
             ans =c
-    if type(ans) == int:
-        return ans
-    else:
-        return max(ans)
+    results.append(ans)
+
 
 print ( max_val(((2, 3, 4), (5, 6), [10, [11]])))
 
