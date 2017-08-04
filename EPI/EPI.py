42ca44399a6d69ebb0bc903ba8b440f13db66c4f
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2017-08-04 13:47:30 -0400

    ch. 5, max profit buy sell stock

---
+++ EPI/EPI.py
@@ -0,0 +1,27 @@
+"""
+Buy and sell one share of stock, given the array of prices. Cannot 
+short stock, (have to buy before selling). Return the max profit possible.
+"""
+# Brute force, use two nested loops, find max from every possible transaction.
+def maxProfit(prices):
+	maxProfit =0
+	for i in range(len(prices)):
+		for j in range(i+1, len(prices)):
+			maxProfit = max(maxProfit, prices[j]-prices[i])
+	return maxProfit
+
+
+#One pass approach
+
+def maxProfit1(prices):
+	maxProfit=0
+	lowestSoFar=100
+	highestSoFar = 0
+	for i in range(len(prices)):
+		if prices[i] < lowestSoFar:
+			lowestSoFar=prices[i]
+		maxProfit= max(maxProfit, prices[i]-lowestSoFar)
+	return maxProfit	
+
+print(maxProfit1([20, 1, 9, 2, 10]))
+
