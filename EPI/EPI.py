3df6f85a855d9c2199b663d7944c3ad9f0822dc4
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2017-08-14 05:58:16 -0400

    number ways make change

--- EPI/EPI.py
+++ EPI/EPI.py
@@ -4,14 +4,40 @@ solutions to problems in EPI
 
 """
 8/14/17
-Find the number of ways to make change
+Find the number of ways to make change, given an amount change and 
+an array of denominations, coins.
+http://www.geeksforgeeks.org/dynamic-programming-set-7-coin-change/
 """
+# Dynamic Programming Python implementation of Coin Change problem
+def count(S, m, n):
+    # We need n+1 rows as the table is consturcted in bottom up
+    # manner using the base case 0 value case (n = 0)
+    table = [[0 for x in range(m)] for x in range(n+1)]
+ 
+    # Fill the enteries for 0 value case (n = 0)
+    for i in range(m):
+        table[0][i] = 1
+ 
+    # Fill rest of the table enteries in bottom up manner
+    for i in range(1, n+1):
+        for j in range(m):
+            # Count of solutions including S[j]
+            x = table[i - S[j]][j] if i-S[j] >= 0 else 0
+ 
+            # Count of solutions excluding S[j]
+            y = table[i][j-1] if j >= 1 else 0
+ 
+            # total count
+            table[i][j] = x + y
+ 
+    return table[n][m-1]
 
 
 """
 8/14/17
-Giving an amount of change and a list of available denominations coins, find the minimum
-number of coins required to make c.
+Giving an amount of change and a list of available denominations, coins, find the minimum
+number of coins required to make c. makeChange is the optimized version of 
+makeChange1, my original solution
 """
 def makeChange(change, coins):
 
