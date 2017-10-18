e04dfc63b835dc9ab3ecf356a0890d615c13abba
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2017-10-18 00:37:48 +0000

    add permutations.py

---
+++ Kangli/backtracking/permutations.py
@@ -0,0 +1,19 @@
+#Given a collection of distinct numbers, return all possible permutations.
+#recursive solution
+class Solution(object):
+    def permute(self, nums):
+        res = []
+        self.permuteHelper( nums, 0, res)
+        return res
+    def permuteHelper(self, nums, start, results):
+                if start >= len(nums):
+                    results.append(nums[:])
+                else:
+                    for i in range(start, len(nums)):
+                        nums[i], nums[start] = nums[start], nums[i]
+                        self.permuteHelper( nums, start +1, results)
+                        nums[start], nums[i] = nums[i], nums[start]
+
+s = Solution()
+arr = [1, 2, 3]
+print s.permute(arr)
