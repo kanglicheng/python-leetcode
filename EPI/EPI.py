329c1b3e9188e68c2c1e7ff00cc96095f4403f79
Steven Cheng <kangli.cheng@gmail.com>
2017-12-27 17:40:48 -0800

    start chapter 5 in EPI

--- EPI/EPI.py
+++ EPI/EPI.py
@@ -1,44 +1,70 @@
 """
 solutions to problems in EPI
 """
+#naive recursion
+def cc(n, denoms):
+	minCoins =100
+	globalMin =100
+	if n == 0:
+		return 0
+	for a in denoms:
+		if a <= n:
+			globalMin = min(1+cc(n-a, denoms), globalMin)
+		#if minCoins < globalMin:
+			#globalMin= minCoins
+	return globalMin
+
+print(cc(5, [1, 5, 10, 25]))
+print(cc(33, [1, 5, 10, 25]))
+
+# greedy algorithm, always pick the largest possible coin, subtract from n
+def greedyCC(n, denoms):
+	prev =0
+	count =0
+	while n != 0:
+		n -= max(denoms) if max(denoms)<n 
+		count +=1 
+	return count
+
+
+
 
 """
 8/14/17
 Find the number of ways to make change, given an amount change and 
 an array of denominations, coins.
 http://www.geeksforgeeks.org/dynamic-programming-set-7-coin-change/
-"""
-# Dynamic Programming Python implementation of Coin Change problem
-def count(S, m, n):
+
+def numberWaysMakeChange(change, coins):
     # We need n+1 rows as the table is consturcted in bottom up
     # manner using the base case 0 value case (n = 0)
-    table = [[0 for x in range(m)] for x in range(n+1)]
+    memo = [[0 for x in range(len(coins))] for _ in range(change+1)]
  
     # Fill the enteries for 0 value case (n = 0)
-    for i in range(m):
-        table[0][i] = 1
+    for i in range(len(coins)):
+        memo[0][i] = 1
  
     # Fill rest of the table enteries in bottom up manner
-    for i in range(1, n+1):
-        for j in range(m):
-            # Count of solutions including S[j]
-            x = table[i - S[j]][j] if i-S[j] >= 0 else 0
+    for i in range(1, change+1):
+        for j in range(len(coins)):
+            # x is number of ways if using coin[j]
+            x = memo[i - coins[j]][j] if i-coins[j] >= 0 else 0
  
-            # Count of solutions excluding S[j]
-            y = table[i][j-1] if j >= 1 else 0
+            # number of ways without using coin[j]
+            y = memo[i][j-1] if j >= 1 else 0
  
             # total count
-            table[i][j] = x + y
+            memo[i][j] = x + y
  
-    return table[n][m-1]
-
-
+    return memo[-1][-1]
+print(numberWaysMakeChange(10, [1, 5, 10, 25]))
+"""
 """
 8/14/17
 Giving an amount of change and a list of available denominations, coins, find the minimum
 number of coins required to make c. makeChange is the optimized version of 
 makeChange1, my original solution
-"""
+
 def makeChange(change, coins):
 
 	memo=[0]+[1000] * (change)
@@ -61,12 +87,12 @@ def makeChange1(change, coins):
 	return memo[-1]
 
 print(makeChange1(25, [1, 2, 5, 10]))
-	
+"""	
 """
 8/4/17
 Buy and sell one share of stock, given the array of prices. Cannot 
 short stock, (have to buy before selling). Return the max profit possible. 
-"""
+
 
 # Brute force, use two nested loops, find max from every possible transaction.
 def maxProfit(prices):
@@ -90,5 +116,5 @@ def maxProfit1(prices):
 	return maxProfit	
 
 print(maxProfit1([20, 1, 9, 2, 10]))
-
+""" 
 
---
+++ EPI/EPI_ch5_arrays.py
@@ -0,0 +1,49 @@
+"""
+Partition an array for quicksort, aka Dutch national flag problem.
+Write a program that takes an array A and an index i into A, and rearranges 
+the elements such that all elements less than A[i] appear first, followed by
+elements equal to the pivot followed by elements greater than the pivot.
+
+Example: A=[-3, 0, -1, 1, 1, 2, 4, 3, 5], i = 4, A is already partitioned correctly.
+12/27/17
+"""
+
+# We can create 3 new lists/arrays, one with all elements < p, another all
+#elements = p, and the third all elements > p. O(n) time but O(n) space also.
+
+def partition_1(arr, i):
+	p = arr[i]
+	a1, a2, a3, = [], [], []
+	for n in arr:
+		if n < p:
+			a1.append(n)
+		elif n == p:
+			a2.append(n)
+		else:
+			a3.append(n)
+	return a1 + a2 + a3
+
+#print partition_1([4, 5, 2, -1, 0, 2, 1], 2)
+#got [-1, 0, 1, 2, 2, 4, 5], correct answer
+
+#first pass groups elements < p, second pass groups elements >p
+# O(1) space and O(n) time
+def partition_opt(arr, i):
+	p = arr[i]
+	smaller = 0
+	for i in range(len(arr)):
+		if arr[i] < p:
+			arr[i], arr[smaller] = arr[smaller], arr[i]
+			smaller += 1
+	larger = len(arr)-1
+	for i in reversed(range(len(arr))):
+		if arr[i] < p:
+			break
+		elif arr[i] > p:
+			arr[i], arr[larger] = arr[larger], arr[i]
+			larger -= 1
+	return arr
+
+print partition_opt([4, 5, 2, -1, 0, 2, 1], 2)
+
+
