07d5a3ea2e3b4a0abe210c1269dd25d06d161fb5
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2018-01-04 19:33:42 -0800

    Update containsDuplicate_II.py

--- Kangli/Hash Table/containsDuplicate_II.py
+++ Kangli/Hash Table/containsDuplicate_II.py
@@ -11,3 +11,13 @@ class Solution(object):
                     d[n]= i
         else:
             return False
+
+# Basically the same solution
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        d = {}
+        for i, v in enumerate(nums):
+            if v in d and i - d[v] <= k:
+                return True
+            d[v] = i
+        return False
