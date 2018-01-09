adc058bf487f122903be2d9efc00a5880bef3b0f
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2018-01-08 22:29:25 -0800

    add my own clumsy solution

--- Kangli/Arrays/pascalTriangle.py
+++ Kangli/Arrays/pascalTriangle.py
@@ -1,3 +1,24 @@
+# not very elegant, but accepted. Created Pascal's directly from definition
+class Solution(object):
+    def generate(self, numRows):
+        ans = [[1], [1, 1]]
+        if numRows == 0:
+            return []
+        elif numRows == 1:
+            return [[1]]
+        elif numRows == 2:
+            return [[1], [1, 1]]
+        else:
+            for i in range(3, numRows +1):
+                temp = [1]*i
+                for j in range(i):
+                    if j ==0 or j == i-1:
+                        continue
+                    else:
+                        temp[j] = ans[i-2][j-1] + ans[i-2][j]
+                ans.append(temp)
+            return ans
+        
 def generate(self, numRows):
         res = [[1]]
         for i in range(1, numRows):
