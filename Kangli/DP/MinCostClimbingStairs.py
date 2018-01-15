a21b1944dd0bfef147e7afc556f1d4bf57d15bc6
Kang-Li (Stephen) Cheng <kangli.cheng@gmail.com>
2018-01-15 00:20:10 -0800

    add O(1) space solution

--- Kangli/DP/MinCostClimbingStairs.py
+++ Kangli/DP/MinCostClimbingStairs.py
@@ -6,3 +6,12 @@ class Solution(object):
         for i in range(2, len(dp)):
             dp[i] = cost[i] + min(dp[i-1], dp[i-2])
         return dp[-1]
+
+# Using O(1) space
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        d1, d2, k = 0, 0, 0
+        for i in range(2, len(cost)+1):
+            k = min(d1 + cost[i-2], d2+ cost[i-1])
+            d1, d2 = d2, k
+        return k
