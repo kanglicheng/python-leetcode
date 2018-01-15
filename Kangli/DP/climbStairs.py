84d8b55eab4b9007380a0960edba5d0f33179f87
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2018-01-15 12:21:31 -0800

    add recent submissions

--- Kangli/DP/climbStairs.py
+++ Kangli/DP/climbStairs.py
@@ -31,6 +31,29 @@ class Solution(object):
     return nw[n]
  climbStairs(5)
 """ 
-    
+
+# 1/14/18 Pure recursion solution, TLE for large n.
+class Solution(object):
+    def climbStairs(self, n):
+        if n == 0:
+            return 0
+        elif n == 1: 
+            return 1
+        elif n ==2:
+            return 2
+        else:
+            return self.climbStairs(n-2) + self.climbStairs(n-1)
+# memoized recursion          
+class Solution(object):
+    def climbStairs(self, n):
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        dp = [0]*(n+1)
+        dp[1], dp[2] = 1, 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
     
     
